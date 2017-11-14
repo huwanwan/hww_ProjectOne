debb718ceaa0b1bb437097e95219ec361e1566eb
huwanwan <hww125@126.com>
2017-11-14 11:31:22 +0800

    新增轮播图插件

--- src/lib/hwCarousel/hwCarousel.js
+++ src/lib/hwCarousel/hwCarousel.js
@@ -2,7 +2,7 @@
 * @Author: Marte
 * @Date:   2017-11-08 21:31:21
 * @Last Modified by:   Marte
-* @Last Modified time: 2017-11-09 11:19:18
+* @Last Modified time: 2017-11-13 12:43:17
 */
 
 ;(function($){
@@ -14,6 +14,7 @@
             opacity:1,
             // 存放图片;
             imgList:[],
+            imgSrc:['javascript:;'],
             // 给一个默认的类名;
             clName:'carousel',
             // 给大图的盒子默认的类名;
@@ -29,9 +30,11 @@
             // 默认自动播放;
             autoplay:true,
             // 默认显示前后点击按钮;
-            btns:true,
+            btns:false,
             // 默认下面的页面小图标显示;
-            pages:true
+            pages:true,
+            pageNum:false,
+            pageSize:{'width':20,'height':20}
         }
         return $(this).each(function(){            
             // 合并参数;遍历调用方法的jq对象类数组;
@@ -47,8 +50,19 @@
                         this.index = opt.index;
                         this.lastIndex = 0;
                         // 根据参数的img数量生成li和img;
-                        this.imgEle.innerHTML = opt.imgList.map(function(item){
-                            return `<li><img src="${item}" alt="" /></li>`;
+                        this.imgEle.innerHTML = opt.imgList.map((item,idx)=>{
+                            if(opt.direction === 'horizontal'){
+                                var len;
+                                if(opt.seamless){
+                                    len = opt.imgList.length+1;
+                                }else{
+                                    len = opt.imgList.length;
+                                }
+                                this.imgEle.style.width = len * opt.width + 'px';
+                                return `<li style="float:left;"><a href="${opt.imgSrc}"><img src="${item}" alt="" /></a></li>`;
+                            }else{
+                                return `<li><a href="${opt.imgSrc}"><img src="${item}" alt="" /></a></li>`; 
+                            }
                         }).join('');
                         // 如果无缝滚动为true,那么克隆第一个子元素,并插入imgUl;
                         if(opt.seamless){
@@ -57,7 +71,7 @@
                             this.imgEle.appendChild(lastChild);
                         } 
                         // 将imgEle插入carousel;
-                        $self.append(this.imgEle);
+                        $self.html(this.imgEle);
                         // 获取imgEle下的所以li节点;
                         this.allImg = this.imgEle.children;
                         // 遍历给每个imgLi添加宽高;
@@ -78,7 +92,6 @@
                             // 横向滚动,imgLi浮动;初始化,根据index的值给ul设置定位置;
                             }else if(opt.direction === 'horizontal'){
                                 this.imgEle.style.left = -this.index * opt.width + 'px';
-                                item.style.float = 'left';
                             }else if(opt.direction === 'vertical'){
                                 this.imgEle.style.top = -this.index * opt.height + 'px';
                             }
@@ -88,18 +101,27 @@
                         if(opt.pages){
                             this.pageEle = document.createElement('ul');
                             this.pageEle.classList.add(opt.showPage);
-                            this.pageEle.innerHTML = opt.imgList.map(item=>{
-                                return `<li></li>`;
+                            this.pageEle.innerHTML = opt.imgList.map((item,idx)=>{
+                                if(opt.pageNum){
+                                    return `<li>${idx+1}</li>`;
+                                }else{
+                                    return `<li></li>`;
+                                }
+                                
                             }).join('');
                             this.pageEle.children[this.index].classList.add('active');
                             $self.append(this.pageEle);
+                            
+                            $(this.pageEle).children('li').css({
+                                'width':opt.pageSize.width,
+                                'height':opt.pageSize.height
+                            });
+
+                            var rightPos = ($self.find('img').width() - $(this.pageEle).width()) - 10;
+                            $(this.pageEle).css('left',rightPos);
                             this.pageEle = this.pageEle.children;
                         }
                         let _this = this;
-                        // 是否自动播放;
-                        if(opt.autoplay){
-                            this.start();
-                        }
                         // 是否创建前后点击按钮;
                         if(opt.btns){
                             this.aNext = document.createElement('span');
@@ -117,21 +139,30 @@
                                 _this.goNext();
                             } 
                         }
-                        
-                        $self.onmouseover = function(){
+                        // 是否自动播放;
+                        if(opt.autoplay){
+                            this.start();
+                        }
+                        $self.on('mouseover','li',function(){
                             _this.aStop();
-                        }
-                        $self.onmouseout = function(){
+                        })
+                        $self.on('mouseout','li',function(){
                             _this.start();
+                        })
+                        if(opt.pages){ 
+                            // 借用数组方法遍历;
+                            Array.prototype.forEach.call(this.pageEle,(item,idx)=>{
+                                item.onmouseover = function(){
+                                    _this.aStop();
+                                    // 把page的下标赋值给index;
+                                    _this.index = idx;
+                                    _this.move();   
+                                }
+                                item.onmouseout = function(){
+                                    _this.start();
+                                }
+                            });
                         }
-                        // 借用数组方法遍历;
-                        Array.prototype.forEach.call(this.pageEle,(item,idx)=>{
-                            item.onclick = function(){
-                                // 把page的下标赋值给index;
-                                _this.index = idx;
-                                _this.move();   
-                            }
-                        });
                     },
                     move(){
                         // 当索引值大于所有li节点长度;
@@ -157,17 +188,19 @@
                             // 判断是否无缝滚动,重置索引值;
                             this.index = opt.seamless ? this.allImg.length - 2 : this.allImg.length - 1;    
                         }
-                        //给所有的页码去掉高亮,给当前加上;
-                        for(var i = 0;i < this.pageEle.length;i++){
-                            this.pageEle[i].classList.remove('active');
-                        }
-                        // 根据this.index给pages添加高亮;
-                        if(this.index < 0){
-                            this.pageEle[this.index-1].classList.add('active')
-                        }else if((this.index > this.allImg.length - 2 && opt.seamless) || (this.index > this.allImg.length - 1)){
-                            this.pageEle[0].classList.add('active');
-                        }else{
-                            this.pageEle[this.index].classList.add('active')
+                        if(opt.pages){
+                            //给所有的页码去掉高亮,给当前加上;
+                            for(var i = 0;i < this.pageEle.length;i++){
+                                this.pageEle[i].classList.remove('active');
+                            }
+                            // 根据this.index给pages添加高亮;
+                            if(this.index < 0){
+                                this.pageEle[this.index-1].classList.add('active')
+                            }else if((this.index > this.allImg.length - 2 && opt.seamless) || (this.index > this.allImg.length - 1)){
+                                this.pageEle[0].classList.add('active');
+                            }else{
+                                this.pageEle[this.index].classList.add('active')
+                            }
                         }
                         let target = {};
                         // 当水平/水平滚动时,设置宽高,算出left/top;
@@ -177,7 +210,6 @@
                             target.top = opt.height * -this.index;
                         }
                         if(opt.direction === 'horizontal'){
-                            this.imgEle.style.width = this.allImg.length * opt.width + 'px';
                             this.imgEle.style.height = opt.height + 'px';
                             target.left = opt.width * -this.index;
                         }
@@ -189,16 +221,14 @@
                             animate(this.allImg[this.index],{opacity:opt.opacity});
                             animate(this.allImg[this.lastIndex],{opacity:0});
                             //记录一个lastIndex;
-                            this.lastIndex = this.index
-                            console.log(this.index,this.lastIndex);
+                            this.lastIndex = this.index;
                             return;
                         }
                         // 执行函数;
                         animate(this.imgEle,target);
-
-                       ;
                     },
                     start(){
+                        clearInterval(this.timer);
                         this.timer = setInterval(()=>{
                             this.index++;
                             this.move();
